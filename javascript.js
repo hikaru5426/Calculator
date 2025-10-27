113ba40ec2db1b94df3a52bc91f7644a65f514d3
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-27 17:18:36 +0100

    Malformed expression handled

--- javascript.js
+++ javascript.js
@@ -17,6 +17,7 @@ const btnDivide = document.querySelector("#btnDivide");
 let number1;
 let number2;
 let operator;
+const listOperator = ["+", "-", "x", "/"];
 
 let currentNumber = "";
 let previousElement;
@@ -71,77 +72,102 @@ function clear() {
 
     pScreen.textContent = "";
     pScreen.valid = true;
+    pMalformedExpression.textContent = "";
 }
 
 function equal() {
-    number2 = Number(currentNumber);
-    switch (operator) {
-        case "+":
-            pScreen.textContent += ` = ${String(add())}`;
-            break;
-        case "-":
-            pScreen.textContent += ` = ${String(subtract())}`;
-            break;
-        case "x":
-            pScreen.textContent += ` = ${String(multiply())}`;
-            break;
-        case "/":
-            pScreen.textContent += ` = ${String(divide())}`;
-            break;
-        default:
-            console.log(`default case met in equal function, operator = ${operator}`);
+    if (checkOperationValid()) {
+        number2 = Number(currentNumber);
+        previousElement = "=";
+        switch (operator) {
+            case "+":
+                pScreen.textContent += ` = ${String(add())}`;
+                break;
+            case "-":
+                pScreen.textContent += ` = ${String(subtract())}`;
+                break;
+            case "x":
+                pScreen.textContent += ` = ${String(multiply())}`;
+                break;
+            case "/":
+                pScreen.textContent += ` = ${String(divide())}`;
+                break;
+            default:
+                console.log(`default case met in equal function, operator = ${operator}`);
+        }
+    }else{
+        pScreen.textContent += "=";
+    }
+
+}
+
+function checkOperationValid() {
+    if (previousElement === "=" || !pScreen.valid) {
+        pMalformedExpression.textContent = "Malformed operation, press Clear to reset";
+        return false;
+    } else {
+        return true
     }
 }
 
 function operatorClicked(event) {
-    if (pScreen.valid) {
+
+    if (checkOperationValid()) {
         if (Number(previousElement) == previousElement && operator === undefined) {
             operator = event.target.textContent;
+
             number1 = Number(currentNumber);
             currentNumber = "";
             pScreen.textContent = `${number1} ${operator}`;
             previousElement = operator;
         } else {
-            pScreen.textContent = "Can't put an operator here, press clear to reset";
+            pMalformedExpression.textContent = "Malformed operation, press Clear to reset";
             pScreen.valid = false;
         }
-    }
+    }else pScreen.textContent += event.target.textContent;
 }
 
 function numberClicked(event) {
-    if (pScreen.valid) {
+
+
+    if (checkOperationValid()) {
         const number = event.target.textContent;
 
+        // 1er chiffre
         if (previousElement === undefined) {
-            currentNumber  = number;
+            currentNumber = number;
             pScreen.textContent = number;
+
+            // Si dernier élément de l'opération était un chiffre
         } else if (Number(previousElement) == previousElement) {
             currentNumber += number
             pScreen.textContent += number;
+
+            // Si dernier élément de l'opération était un opérateur
         } else {
             currentNumber = number;
             pScreen.textContent = `${number1} ${operator} ${currentNumber}`;
         }
 
         previousElement = currentNumber;
-    }
+    }else pScreen.textContent += event.target.textContent;
 }
 
-    function createPage() {
-        for (let i = 9; i >= 0; i--) {
-            const btn = document.createElement("button");
-            btn.textContent = String(i);
-            btn.addEventListener("click", numberClicked)
-            divBtnNumbers.appendChild(btn);
-        }
-
-        btnClear.addEventListener("click", clear);
-        btnEqual.addEventListener("click", equal);
-
-        btnOperations.childNodes.forEach(node => {
-            node.addEventListener("click", operatorClicked);
-        })
-
+function createPage() {
+    for (let i = 9; i >= 0; i--) {
+        const btn = document.createElement("button");
+        btn.textContent = String(i);
+        btn.addEventListener("click", numberClicked)
+        divBtnNumbers.appendChild(btn);
     }
 
-    createPage();
+    btnClear.addEventListener("click", clear);
+    btnEqual.addEventListener("click", equal);
+
+    btnOperations.childNodes.forEach(node => {
+        node.addEventListener("click", operatorClicked);
+    })
+
+}
+
+createPage();
